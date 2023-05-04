6e9c533e8c2240f8ef0c910ece6ea64591f839fc
Shivam <65666800+Leon-96@users.noreply.github.com>
2023-05-04 15:43:02 +0530

    Update Day1-youtube_player.py

--- Day1-youtube_player.py
+++ Day1-youtube_player.py
@@ -11,4 +11,4 @@ youtube_player()
 
 
 #  to run this script in background in linux you can use crontab to run it as a daemon in background so  add this line 
-# 0 * * * * /path of Da1-youtube_player.py to run the code in the background 
+# 0 * * * * /path of Day1-youtube_player.py to run the code in the background 
