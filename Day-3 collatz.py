5a6e779306bf0e928a8594620966e4523b11f8c9
Shivam <65666800+Leon-96@users.noreply.github.com>
2023-05-04 15:33:01 +0530

    Update Day-3 collatz.py

--- Day-3 collatz.py
+++ Day-3 collatz.py
@@ -1,15 +1,21 @@
+def collatz_checker(n):
+    if n<1:
+        print("Not Valid")
+
+    if n>1:
+        while n>1:
+            
+            if n%2!=0:
+                n = (3*n) +1 
+                print(int(n))
+
+            elif n%2==0:
+                n /= 2
+                print(int(n))
+
+            
+        print("Collatz Sequence Verified")
+
 n = int(input("Enter starting point for collatz sequence : "))
 
-
-
-while n!=1:
-    
-    if n%2!=0:
-        n = 3*n +1 
-        print(int(n))
-
-    elif n%2==0:
-        n /= 2
-        print(int(n))
-
-    
+collatz_checker(n)
