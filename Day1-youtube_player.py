7b51a3fe45b2f511628b6550f26f99229280775d
Shivam <65666800+Leon-96@users.noreply.github.com>
2023-05-04 15:42:35 +0530

    Add files via upload

---
+++ Day1-youtube_player.py
@@ -0,0 +1,14 @@
+import time
+import webbrowser
+
+def youtube_player():
+    link = "https://www.youtube.com/watch?v=xJK3FrOHoxg"
+    webbrowser.open_new(link)
+
+    time.sleep(3600)
+
+youtube_player()
+
+
+#  to run this script in background in linux you can use crontab to run it as a daemon in background so  add this line 
+# 0 * * * * /path of Da1-youtube_player.py to run the code in the background 
